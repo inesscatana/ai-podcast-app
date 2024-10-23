2549053dbfa34d8f347026a863b4fd54ba81a66f
inesscatana <ines.arts@hotmail.com>
2024-10-23 14:12:50 +0100

    Right sidebar

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
 				protocol: 'https',
 				hostname: 'https://fast-wren-739.convex.cloud',
 			},
+			{
+				protocol: 'https',
+				hostname: 'img.clerk.com',
+			},
 		],
 	},
 }
